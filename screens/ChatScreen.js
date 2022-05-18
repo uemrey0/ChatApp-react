530682d13f06597ed8a804af49077d1eabba36b5
uemrey0 <ufukemreyuceturk@hotmail.com>
2022-05-18 18:49:09 +0300

    Chat screen func

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,14 @@
-import { KeyboardAvoidingView, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { Keyboard, KeyboardAvoidingView, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'
 import React, { useLayoutEffect, useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { Avatar } from 'react-native-elements';
 import {AntDesign, Ionicons, FontAwesome} from "@expo/vector-icons";
+import { auth, db } from '../firebase';
+import firebase from "firebase/compat/app";
 
 const ChatScreen = ({navigation, route}) => {
     const [input, setInput] = useState("");
+    const [messages, setMessages] = useState([]);
     useLayoutEffect(() => {
         navigation.setOptions({
             title: "Chat",
@@ -13,7 +16,7 @@ const ChatScreen = ({navigation, route}) => {
             headerTitleAlign: 'left',
             headerTitle: () => (
                 <View style={{flexDirection: "row", alignItems: "center",}}>
-                    <Avatar rounded source={{uri: "https://www.seekpng.com/png/detail/110-1100707_person-avatar-placeholder.png"}} />
+                    <Avatar rounded source={{uri: messages[0]?.data.photoURL}} />
                     <Text style={{color: "white", marginLeft: 10, fontWeight: "700"}}>{route.params.chatName}</Text>
                 </View>
             ),
@@ -33,21 +36,83 @@ const ChatScreen = ({navigation, route}) => {
                 </View>
             ),
         })
-    }, [navigation]); 
+    }, [navigation, messages]); 
     const sendMessage = () => {
-
+        db.collection("chats").doc(route.params.id).collection("messages").add({
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            message: input,
+            displayName: auth.currentUser.displayName,
+            email: auth.currentUser.email,
+            photoURL: auth.currentUser.photoURL,
+        });
+        setInput("");
     };
+    useLayoutEffect(() => {
+        const unsubscribe = db
+        .collection("chats")
+        .doc(route.params.id)
+        .collection("messages")
+        .orderBy("timestamp", "desc")
+        .onSnapshot(snapshot => setMessages(snapshot.docs.map(doc => ({
+            id: doc.id,
+            data: doc.data(),
+        }))));
+        return unsubscribe;
+    }, [route]);
     return (
         <SafeAreaView style={{flex:1, backgroundColor: "white"}}>
             <StatusBar style='light'/>
             <KeyboardAvoidingView
             behavior={Platform.OS == "ios" ? "padding" : "height"}
             style={styles.container}
+            enabled
             keyboardVerticalOffset= {90}
             >
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss()}>
                 <>
-                    <ScrollView>
-                        {/** chat is here */}
+                    <ScrollView contentContainerStyle={{paddingTop: 15}}>
+                        {messages.map(({id,data})=>(
+                            data.email === auth.currentUser.email ? (
+                                <View key={id} style={styles.reciever}>
+                                    <Avatar 
+                                    position="absolute"
+                                    rounded
+                                    containerStyle={{
+                                        position: "absolute",
+                                        bottom: -15,
+                                        right: -5,
+                                    }}
+                                    bottom={-15}
+                                    right={-5}
+                                    size={30}
+                                    source={{
+                                        uri: data.photoURL
+                                    }}
+                                    />
+                                    <Text style={styles.recieverText}>{data.message}</Text>
+                                </View>
+                            ) : (
+                                <View key={id} style={styles.sender}>
+                                    <Avatar 
+                                    position="absolute"
+                                    rounded
+                                    containerStyle={{
+                                        position: "absolute",
+                                        bottom: -15,
+                                        left: -5,
+                                    }}
+                                    bottom={-15}
+                                    left={-5}
+                                    size={30}
+                                    source={{
+                                        uri: data.photoURL
+                                    }}
+                                    />
+                                    <Text style={styles.senderText}>{data.message}</Text>
+                                    <Text style={styles.senderName}>{data.displayName}</Text>
+                                </View>
+                            )
+                        ))}
                     </ScrollView>
                     <View style={styles.footer}>
                         <TextInput placeholder='Type a massage ...' style={styles.textInput} value={input} onChangeText={(text)=>setInput(text)} />
@@ -57,6 +122,7 @@ const ChatScreen = ({navigation, route}) => {
                     </View>
                     
                 </>
+                </TouchableWithoutFeedback>
             </KeyboardAvoidingView>
         </SafeAreaView>
     )
@@ -67,7 +133,6 @@ export default ChatScreen
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-
     },
     footer: {
         flexDirection: "row",
@@ -79,10 +144,48 @@ const styles = StyleSheet.create({
         bottom: 0,
         height: 40,
         flex: 1,
-        marginRight: 15,
+        marginRight: 15, 
         backgroundColor: "#ECECEC",
         padding: 10,
         color: "grey",
         borderRadius: 30
+    },
+    reciever: {
+        padding: 15,
+        backgroundColor: "#ECECEC",
+        alignSelf: "flex-end",
+        borderRadius: 20,
+        marginRight: 15,
+        marginBottom: 20,
+        maxWidth: "80%",
+        position: "relative",
+    },
+    recieverText: {
+        color: "black",
+        fontWeight: "500",
+        marginLeft: 10,
+        marginBottom: 15,
+    },
+    sender: {
+        padding: 15,
+        backgroundColor: "#ECECEC",
+        alignSelf: "flex-start",
+        borderRadius: 20,
+        marginRight: 15,
+        marginBottom: 20,
+        maxWidth: "80%",
+        position: "relative",
+    },
+    senderText: {
+        color: "white",
+        fontWeight: "500",
+        marginLeft: 10,
+        marginBottom: 15,
+    },
+    senderName: {
+        left: 10,
+        paddingRight: 10,
+        fontSize: 10,
+        color: "white",
     },
 })
