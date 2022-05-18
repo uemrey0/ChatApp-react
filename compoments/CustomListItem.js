943ded8c3b3d0ae6c5224c4b720efaaa3a75101a
uemrey0 <ufukemreyuceturk@hotmail.com>
2022-05-18 19:17:13 +0300

    Home screen update chat info

--- compoments/CustomListItem.js
+++ compoments/CustomListItem.js
@@ -1,18 +1,31 @@
 import { StyleSheet, Text, View } from 'react-native'
 import { ListItem, Avatar, } from 'react-native-elements'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ListItemContent } from '@rneui/base/dist/ListItem/ListItem.Content'
+import { db } from '../firebase'
 
 const CustomListItem = ({id,chatName,enterChat}) => {
+    const [chatMessages, setChatMessages] = useState([]);
+    useEffect(() => {
+        const unsubscribe = db
+        .collection("chats")
+        .doc(id)
+        .collection("messages")
+        .orderBy("timestamp", "desc")
+        .onSnapshot((snapshot) => setChatMessages(snapshot.docs.map((doc) => doc.data())));
+        return unsubscribe;
+    });
   return (
     <ListItem onPress={() => enterChat(id,chatName)} key={id} bottomDivider>
-        <Avatar rounded source={{uri: "https://www.seekpng.com/png/detail/110-1100707_person-avatar-placeholder.png"}} />
+        <Avatar rounded source={{uri: chatMessages?.[0]?.photoURL || "https://www.seekpng.com/png/detail/110-1100707_person-avatar-placeholder.png"}} />
         <ListItem.Content>
             <ListItem.Title style={{fontWeight: "800"}}>
                 {chatName}
             </ListItem.Title>
             <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
-                This is a Subtitle
+                {
+                    chatMessages?.[0]?.displayName === undefined ? "" : chatMessages?.[0]?.displayName+": "+chatMessages?.[0]?.message
+                }
             </ListItem.Subtitle>
         </ListItem.Content>
     </ListItem>
