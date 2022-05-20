c4d14c15a52aae749fc70c3580469fc46605d71c
uemrey0 <ufukemreyuceturk@hotmail.com>
2022-05-20 23:57:53 +0300

    Chat start opt. fecth contact

--- screens/AddChatScreen.js
+++ screens/AddChatScreen.js
@@ -1,10 +1,42 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React, { useLayoutEffect, useState } from 'react'
+import React, { useLayoutEffect, useState, useEffect } from 'react'
 import { Button, Icon, Input } from 'react-native-elements'
+import * as Contacts from 'expo-contacts';
 import { db } from '../firebase';
 
 const AddChatScreen = ({navigation}) => {
     const [input, setInput] = useState("");
+    const dataJSON = require('../assets/CountryCodes.json');
+    var contactPhones = [];
+    var countrCodes = [];
+    var mergedPhone = [];
+    useEffect(() => {
+        (async () => {
+          const { status } = await Contacts.requestPermissionsAsync();
+          if (status === 'granted') {
+            const { data } = await Contacts.getContactsAsync({
+              fields: [Contacts.Fields.PhoneNumbers],
+            });
+    
+            if (data.length > 0) {
+              
+              for(var i = 0; i<data.length; i++){
+                contactPhones[i] = data[i]["phoneNumbers"][0]["digits"];
+                var code = data[i]["phoneNumbers"][0]["countryCode"].toUpperCase();
+                for (var j = 0; j<dataJSON.length; j++){
+                    if(dataJSON[j].code === code){
+                        countrCodes[i] = dataJSON[j].dial_code;
+                    }
+                }
+                
+              }
+              for(var i = 0; i<contactPhones.length; i++){
+                  mergedPhone[i] = countrCodes[i]+contactPhones[i];
+              }
+            }
+          }
+        })();
+      }, []);
     useLayoutEffect(() => {
         navigation.setOptions({
             headerBackTitle: "Chats",
