69a0acfc4d43b7f2a7f514cc035443ffaa1895b8
uemrey0 <ufukemreyuceturk@hotmail.com>
2022-05-24 19:04:58 +0300

    Fix: Auth persistence

--- firebase.js
+++ firebase.js
@@ -3,6 +3,9 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 import  {privateConfig} from "./privateConfig";
+import { getReactNativePersistence } from "firebase/auth/react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { initializeAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: privateConfig.apiKey,
@@ -16,6 +19,9 @@ const firebaseConfig = {
 let app;
 if (firebase.apps.length === 0){
     app = firebase.initializeApp(firebaseConfig);
+    initializeAuth(app, {
+      persistence: getReactNativePersistence(AsyncStorage)
+    });
 }else{
     app = firebase.app();
 }
